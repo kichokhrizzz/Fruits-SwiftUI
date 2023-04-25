f178024073389e5c27517d8e6f68720589c6f261
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-04-25 11:12:04 -0600

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Jhosel Badillo Cortes on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
